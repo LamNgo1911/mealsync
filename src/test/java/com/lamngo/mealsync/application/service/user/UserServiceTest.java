b583cdfa18c2ca9cd2e3ddbf0f0f1105d4531a76
Diddayngo <lamngo606@gmail.com>
2025-05-25 16:14:00 +0300

    Update: added getAllUsers_success test

---
+++ src/test/java/com/lamngo/mealsync/application/service/user/UserServiceTest.java
@@ -0,0 +1,44 @@
+package com.lamngo.mealsync.application.service.user;
+
+import com.lamngo.mealsync.application.dto.user.UserReadDto;
+import com.lamngo.mealsync.application.mapper.user.UserMapper;
+import com.lamngo.mealsync.application.mapper.user.UserPreferenceMapper;
+import com.lamngo.mealsync.domain.model.user.User;
+import com.lamngo.mealsync.domain.repository.user.IUserRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class UserServiceTest {
+    @Mock private IUserRepo userRepo;
+    @Mock private UserMapper userMapper;
+    @Mock private UserPreferenceMapper userPreferenceMapper;
+    @InjectMocks private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        userService = new UserService(userRepo, userPreferenceMapper, userMapper);
+    }
+
+    @Test
+    void getAllUsers_success() {
+        User user = new User();
+        UserReadDto userReadDto = new UserReadDto();
+        // Mock the expected behavior
+        when(userRepo.findAll()).thenReturn(List.of(user));
+        when(userMapper.toUserReadDto(any())).thenReturn(userReadDto);
+
+        List<UserReadDto> users = userService.getAllUsers();
+        // Verify the result
+        assertEquals(List.of(userReadDto), users);
+    }
+}
